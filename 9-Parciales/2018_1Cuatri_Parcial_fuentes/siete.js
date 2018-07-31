f9924638b42ea9c24352342f1ad93d2dabe6b490
cotemanuel1997 <40776566+cotemanuel1997@users.noreply.github.com>
2018-07-30 22:10:19 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,54 @@
+/*JOSE M 25
+
+MARIA F 16
+
+JESUS M 33
+
+FER F 81// validar sexo y edad
+
+
+
+*/
 function mostrar()
 {
+	var sexo;
+	var edad;
+	var contador;
+	var nombre;
 
-}
+	contador=0;
+
+	while(contador<4)
+	{
+		contador++;
+		nombre=prompt("Ingrese nombre valido");
+
+		sexo=prompt("Ingrese sexo valido");
+		while(sexo!="m"&&sexo!="f")
+		{
+			sexo=prompt("Ingrese sexo valido");
+		}
+
+		edad=prompt("Ingrese edad");
+		edad=parseInt(edad);
+		while(isNaN(edad)||edad<0||edad>100)
+		{
+			edad=prompt("Ingrese edad");
+			edad=parseInt(edad);
+		}
+
+
+
+
+
+	}
+
+
+	
+	document.write(contadorDeMujeres);
+	document.write(conadorDeHombres);
+
+}	
+
+
+
